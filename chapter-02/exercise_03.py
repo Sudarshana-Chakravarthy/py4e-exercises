998c40d51d4729d4c952198536f3d0948c28d14e
Sudi <sudarshanachakravrthy@gmail.com>
2025-07-04 20:52:57 +0530

    Completed Chapter 2 exercises

---
+++ chapter-02/exercise_03.py
@@ -0,0 +1,6 @@
+# a program to compute gross pay
+
+hours = int(input("Enter Hours: "))
+rate = float(input("Ener Rate: "))
+pay = hours * rate
+print(f"Pay: : {pay}")
---
+++ chapter-02/exercise_04.py
@@ -0,0 +1,19 @@
+width = 17
+height = 12.0
+
+exp1 = width//2
+exp2 = width/2.0
+exp3 = height/3
+exp4 = 1+2*5
+
+print(f"{exp1}")
+print(type(exp1))
+
+print(f"{exp2}")
+print(type(exp2))
+
+print(f"{exp3}")
+print(type(exp3))
+
+print(f"{exp4}")
+print(type(exp4))
